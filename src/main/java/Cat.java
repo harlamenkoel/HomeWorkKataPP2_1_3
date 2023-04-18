2284f41ccefe55d1200d0c256d10d24096825b9a
harlamenkoel <harlamenkoel@gmail.com>
2023-04-18 15:19:05 +0700

    Выполнил

---
+++ src/main/java/Cat.java
@@ -0,0 +1,11 @@
+public class Cat {
+    private String massage;
+
+    public String getMassage() {
+        return massage;
+    }
+
+    public void setMessage(String massage) {
+        this.massage = massage;
+    }
+}
